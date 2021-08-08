6145bb59e6483552b4191ddb062d224b24de1933
chinjja <chinjja@gmail.com>
2021-08-08 12:46:39 +0900

    ValidationAdvice

---
+++ src/main/java/com/chinjja/app/web/ValidationAdvice.java
@@ -0,0 +1,15 @@
+package com.chinjja.app.web;
+
+import javax.validation.ValidationException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ValidationAdvice {
+	@ExceptionHandler(ValidationException.class)
+	public ResponseEntity<?> handle(ValidationException e) {
+		return ResponseEntity.badRequest().body(e.getMessage());
+	}
+}
